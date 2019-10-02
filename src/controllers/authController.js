e95124a4c8bbfa8de6f56573d0374d6f3887aecd
Joalbert <joalbertgonzalez@gmail.com>
2019-10-02 16:29:47 -0500

    add bcrypt to password and user persistence

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,12 +5,11 @@ const User = require('../models/User');
 
 router.post('/signup', async (req, res, next) => {
   const { username, email, password } = await req.body;
-  const user = new User({
-    username,
-    email,
-    password
-  });
-  res.json(user);
+  const user = new User({ username, email });
+  user.password = await user.encryptPassword(password);
+  await user.save();
+
+  res.json({ message: 'User created'});
 });
 
 router.post('/signin', (req, res, next) => {
